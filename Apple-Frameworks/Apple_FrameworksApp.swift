7772d0bd7e881c2ac40d5b87e8355ea65d24eb93
Ivan Trj <ivantrajanovski@gmail.com>
2022-01-04 17:39:23 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Ivan Trajanovski on 2022. 01. 04..
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
